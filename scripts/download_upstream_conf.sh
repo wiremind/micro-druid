f643a10760725b26af824d961219c25c09c1874e
maxime1907 <19607336+maxime1907@users.noreply.github.com>
2024-05-16 18:31:12 +0200

    chore(conf): use upsteam conf

---
+++ scripts/download_upstream_conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/apache/druid.git"
+
+BRANCH_OR_TAG="26.0.1"
+
+CURRENT_DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+
+CLONE_DIR="/tmp/druid-${BRANCH_OR_TAG}-${CURRENT_DATE}"
+
+git clone --depth 1 --branch "$BRANCH_OR_TAG" "$REPO_URL" "$CLONE_DIR"
+
+cp -R "$CLONE_DIR/examples/conf" conf
